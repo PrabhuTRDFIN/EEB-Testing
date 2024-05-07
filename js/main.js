195b0140386b7837397c8f0737cf2f93a691aa6e
prabhuTRDFIN <prabhu.v@trdfin.in>
2024-05-07 12:15:23 +0530

    final

--- js/main.js
+++ js/main.js
@@ -63,7 +63,7 @@
         }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        $('html, body').animate({scrollTop: 0});
         return false;
     });
 
@@ -151,14 +151,13 @@ $("#form").validate({
     messages: {
         Fname: {
             required: "Please enter your name",
-        minlength: "Name at least 2 characters"
+            minlength: "Name at least 2 characters"
         },
         email: "Please enter your email",
         phone: "Please enter your number",
         Lname: "",
         Message: ""
     },
-
 
     submitHandler: function(form) {
       form.submit();
